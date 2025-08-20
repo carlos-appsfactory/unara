9f529289c15223f80bf7d079624f01991dc68396
Carlos <carlos.appsfactory@gmail.com>
2025-08-20 18:46:32 +0200

    Create luggage entity

--- src/luggage/entities/luggage.entity.ts
+++ src/luggage/entities/luggage.entity.ts
@@ -1,1 +1,33 @@
-export class Luggage {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+export enum LuggageType {
+  SMALL_BACKPACK = "mochila pequeña",
+  LARGE_BACKPACK = "mochila grande",
+  SMALL_SUITCASE = "maleta pequeña",
+  LARGE_SUITCASE = "maleta grande",
+}
+
+@Entity()
+export class Luggage {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string
+
+    @Column('text')
+    name: string
+
+    @Column({
+        type: "enum",
+        enum: LuggageType,
+    })
+    type: LuggageType
+
+    // TODO viaje asociado
+    // TODO usuario asociado
+    
+    @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+    createdAt: Date;
+
+    @UpdateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+    updatedAt: Date;
+}
