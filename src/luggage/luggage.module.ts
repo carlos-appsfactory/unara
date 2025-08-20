89fd63d598bcc9d4f70bb31ea8dd6afed3a9305f
Carlos <carlos.appsfactory@gmail.com>
2025-08-20 18:54:05 +0200

    Add Luggage entity to Luggage module

--- src/luggage/luggage.module.ts
+++ src/luggage/luggage.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { LuggageService } from './luggage.service';
 import { LuggageController } from './luggage.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Luggage } from './entities/luggage.entity';
+
 
 @Module({
   controllers: [LuggageController],
   providers: [LuggageService],
+  imports: [
+    TypeOrmModule.forFeature([ Luggage ])
+  ]
 })
 export class LuggageModule {}
