d39b74f517693cd2327b7049760a1670fba4fd58
Carlos <carlos.appsfactory@gmail.com>
2025-08-20 19:25:09 +0200

    Configure create endpoint for luggage

--- src/luggage/luggage.service.ts
+++ src/luggage/luggage.service.ts
@@ -1,11 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
 import { CreateLuggageDto } from './dto/create-luggage.dto';
 import { UpdateLuggageDto } from './dto/update-luggage.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Luggage } from './entities/luggage.entity';
 
 @Injectable()
 export class LuggageService {
-  create(createLuggageDto: CreateLuggageDto) {
-    return 'This action adds a new luggage';
+
+  private readonly logger = new Logger('LuggageService')
+  
+  constructor(
+    @InjectRepository(Luggage)
+    private readonly luggageRepository: Repository<Luggage>
+  ){}
+
+  async create(createLuggageDto: CreateLuggageDto) {
+    try {
+      const luggage = this.luggageRepository.create(createLuggageDto)
+      await this.luggageRepository.save(luggage)
+      return luggage
+
+    } catch (error) {
+      this.handleExceptions(error)
+    }
   }
 
   findAll() {
@@ -22,5 +40,14 @@ export class LuggageService {
 
   remove(id: number) {
     return `This action removes a #${id} luggage`;
+  }
+
+  private handleExceptions(error: any){
+    // TODO: Añadir los códigos de error que veamos que se van dando
+    // if (error.code === 0) throw new BadRequestException(error.detail)
+
+    this.logger.error(error)
+
+    throw new InternalServerErrorException('Unexpected error, check server logs')
   }
 }
