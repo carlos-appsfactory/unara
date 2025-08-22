148fde109ad8484c42f0d04d3b3076512d9d66ac
Carlos <carlos.appsfactory@gmail.com>
2025-08-22 19:17:20 +0200

    Create filter dto for users

---
+++ src/users/dto/filter-user.dto.ts
@@ -0,0 +1,14 @@
+import { IsOptional, IsString, MaxLength } from "class-validator"
+import { PaginationDto } from "src/common/dto/pagination.dto"
+
+export class FilterUserDto extends PaginationDto{
+    @IsOptional()
+    @IsString()
+    @MaxLength(255)
+    email?: string
+    
+    @IsOptional()
+    @IsString()
+    @MaxLength(255)
+    username?: string
+}
