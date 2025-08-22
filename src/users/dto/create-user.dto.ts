dc6f0868e343158a03fe4de5e2b13f0dbc6a180f
Carlos <carlos.appsfactory@gmail.com>
2025-08-22 19:13:31 +0200

    Create users dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,25 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsOptional, IsString, MaxLength } from "class-validator"
+
+export class CreateUserDto {
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(255)
+    fullname: string
+
+    @IsEmail()
+    @MaxLength(255)
+    email: string
+    
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(255)
+    username: string
+
+    @IsString()
+    @IsNotEmpty()
+    password: string
+
+    @IsString()
+    @IsOptional()
+    profile_picture?: string
+}
