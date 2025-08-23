ac26894ae1e8741f50557a43dd16df862cc47960
Carlos <carlos.appsfactory@gmail.com>
2025-08-23 20:07:20 +0200

    Create items entity

--- src/items/entities/item.entity.ts
+++ src/items/entities/item.entity.ts
@@ -1,1 +1,22 @@
-export class Item {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class Item {
+    @PrimaryGeneratedColumn('uuid')
+    id: string
+
+    @Column({ type: 'varchar', length: 255 })
+    name: string
+
+    @Column('text')
+    description: string
+
+    // TODO: Relacionarlo usuarios
+    // TODO: Relacionarlo con maletas
+
+    @CreateDateColumn()
+    createdAt: Date
+
+    @UpdateDateColumn()
+    updatedAt: Date
+}
