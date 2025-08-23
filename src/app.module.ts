99b83f194015c89a899791fae22a644b1e6ae916
Carlos <carlos.appsfactory@gmail.com>
2025-08-23 19:56:58 +0200

    Create items module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { LuggageModule } from './luggage/luggage.module';
 import { CommonModule } from './common/common.module';
 import { UsersModule } from './users/users.module';
+import { ItemsModule } from './items/items.module';
 
 @Module({
   imports: [
@@ -24,7 +25,9 @@ import { UsersModule } from './users/users.module';
 
     CommonModule,
 
-    UsersModule
+    UsersModule,
+
+    ItemsModule
   ],
 })
 export class AppModule {}
