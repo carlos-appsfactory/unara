b5852309fdea682523dca2e24f5b4f2702ddcb22
Carlos <carlos.appsfactory@gmail.com>
2025-08-24 12:27:47 +0200

    Add forFeature for luggage category to luggage module

--- src/luggage/luggage.module.ts
+++ src/luggage/luggage.module.ts
@@ -3,13 +3,14 @@ import { LuggageService } from './luggage.service';
 import { LuggageController } from './luggage.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Luggage } from './entities/luggage.entity';
+import { LuggageCategory } from './entities/luggage-category.entity';
 
 
 @Module({
   controllers: [LuggageController],
   providers: [LuggageService],
   imports: [
-    TypeOrmModule.forFeature([ Luggage ])
+    TypeOrmModule.forFeature([ Luggage, LuggageCategory ])
   ]
 })
 export class LuggageModule {}
