d379b1dd995f37ccc8e38e904b8cf5e2dda70a3d
Carlos <carlos.appsfactory@gmail.com>
2025-08-26 12:19:19 +0200

    Create luggage-categories entity

--- src/luggage-categories/entities/luggage-category.entity.ts
+++ src/luggage-categories/entities/luggage-category.entity.ts
@@ -1,1 +1,22 @@
-export class LuggageCategory {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class LuggageCategory {
+    @PrimaryGeneratedColumn('uuid')
+    id: string
+
+    @Column('text')
+    name: string
+
+    @Column({ type: 'text', nullable: true })
+    description?: string
+
+    @Column('text')
+    image: string
+
+    @CreateDateColumn()
+    createdAt: Date
+
+    @UpdateDateColumn()
+    updatedAt: Date
+}
