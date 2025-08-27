9b732d5eccb59025c5270c1038be5a674fbb20e8
Carlos <carlos.appsfactory@gmail.com>
2025-08-27 11:50:30 +0200

    Create create-luggage-category DTO

--- src/luggage-categories/dto/create-luggage-category.dto.ts
+++ src/luggage-categories/dto/create-luggage-category.dto.ts
@@ -1,1 +1,12 @@
-export class CreateLuggageCategoryDto {}
+import { IsNotEmpty, IsOptional, IsString } from "class-validator";
+
+export class CreateLuggageCategoryDto {
+    @IsString()
+    @IsNotEmpty()
+    name: string
+
+    @IsOptional()
+    @IsString()
+    @IsNotEmpty()
+    description?: string
+}
