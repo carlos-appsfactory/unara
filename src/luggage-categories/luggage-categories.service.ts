4a4fb9744ba76d59dc0d2b2efefdb0e7faab779b
Carlos <carlos.appsfactory@gmail.com>
2025-08-27 11:47:54 +0200

    Create service for luggage-categories

--- src/luggage-categories/luggage-categories.service.ts
+++ src/luggage-categories/luggage-categories.service.ts
@@ -1,26 +1,95 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
 import { CreateLuggageCategoryDto } from './dto/create-luggage-category.dto';
 import { UpdateLuggageCategoryDto } from './dto/update-luggage-category.dto';
+import { LuggageCategory } from './entities/luggage-category.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { FilterLuggageCategoryDto } from './dto/filter-luggage.dto';
 
 @Injectable()
 export class LuggageCategoriesService {
-  create(createLuggageCategoryDto: CreateLuggageCategoryDto) {
-    return 'This action adds a new luggageCategory';
+  private readonly logger = new Logger('LuggageService')
+
+  constructor(
+    @InjectRepository(LuggageCategory)
+    private readonly luggageCategoryRepository: Repository<LuggageCategory>,
+  ){}
+
+  async create(createLuggageCategoryDto: CreateLuggageCategoryDto) {
+    try {
+      const luggageCategory = this.luggageCategoryRepository.create(createLuggageCategoryDto)
+      await this.luggageCategoryRepository.save(luggageCategory)
+      return luggageCategory
+
+    } catch (error) {
+      this.handleExceptions(error)
+    }
   }
 
-  findAll() {
-    return `This action returns all luggageCategories`;
+  async findAll(filterLuggageCategoryDto: FilterLuggageCategoryDto) {
+    const { 
+      limit = 10, 
+      offset = 0,
+      name,
+      description
+    } = filterLuggageCategoryDto
+
+    const query = this.luggageCategoryRepository.createQueryBuilder('item')
+
+    if (name) query.andWhere('luggageCategory.name ILIKE :name', { name: `%${name}%`})
+
+    if (description) query.andWhere('luggageCategory.description ILIKE :description', { description: `%${description}%`})
+
+    query.skip(offset).take(limit)
+
+    return query.getMany()
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} luggageCategory`;
+  async findOne(id: string) {
+    const luggageCategory = await this.luggageCategoryRepository.findOneBy({ id })
+
+    if (!luggageCategory){
+      throw new NotFoundException(`Luggage category with id ${id} not found`)
+    }
+
+    return luggageCategory
   }
 
-  update(id: number, updateLuggageCategoryDto: UpdateLuggageCategoryDto) {
-    return `This action updates a #${id} luggageCategory`;
+  async update(id: string, updateLuggageCategoryDto: UpdateLuggageCategoryDto) {
+    const luggageCategory = await this.luggageCategoryRepository.preload({
+      id,
+      ...updateLuggageCategoryDto
+    })
+
+    if (!luggageCategory){
+      throw new NotFoundException(`Luggage category with id ${id} not found`)
+    }
+
+    try {
+      await this.luggageCategoryRepository.save(luggageCategory)
+      return luggageCategory
+    
+    } catch (error) {
+      this.handleExceptions(error)
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} luggageCategory`;
+  async remove(id: string) {
+    const luggageCategory = await this.luggageCategoryRepository.findOneBy({ id })
+
+    if (!luggageCategory){
+      throw new NotFoundException(`Luggage category with id ${id} not found`)
+    }
+
+    this.luggageCategoryRepository.remove(luggageCategory)
+  }
+
+  private handleExceptions(error: any){
+    // TODO: Añadir los códigos de error que veamos que se van dando
+    // if (error.code === 0) throw new BadRequestException(error.detail)
+
+    this.logger.error(error)
+
+    throw new InternalServerErrorException('Unexpected error, check server logs')
   }
 }
