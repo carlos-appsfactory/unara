165728f60d3125f9b9d2c5ee71ae901b470092fc
Carlos <carlos.appsfactory@gmail.com>
2025-08-27 13:02:41 +0200

    Add import of categories to items module

--- src/items/items.module.ts
+++ src/items/items.module.ts
@@ -3,12 +3,14 @@ import { ItemsService } from './items.service';
 import { ItemsController } from './items.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Item } from './entities/item.entity';
+import { ItemCategoriesModule } from 'src/item-categories/item-categories.module';
 
 @Module({
   controllers: [ItemsController],
   providers: [ItemsService],
   imports: [
-    TypeOrmModule.forFeature([ Item])
+    TypeOrmModule.forFeature([ Item]),
+    ItemCategoriesModule
   ]
 })
 export class ItemsModule {}
