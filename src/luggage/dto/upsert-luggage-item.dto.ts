3429d435c3024566492be24366e76620f243528e
Carlos <carlos.appsfactory@gmail.com>
2025-08-28 13:28:13 +0200

    Set quantity minimum 1 for upser-luggage-item DTO

--- src/luggage/dto/upsert-luggage-item.dto.ts
+++ src/luggage/dto/upsert-luggage-item.dto.ts
@@ -1,8 +1,9 @@
 import { Type } from "class-transformer";
-import { IsInt } from "class-validator";
+import { IsInt, Min } from "class-validator";
 
 export class UpsertLuggageItemDto {
   @IsInt()
+  @Min(1)
   @Type(() => Number)
   quantity: number;
 }
