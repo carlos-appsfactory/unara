c5b461c1a09519f75d380511dd89f25ba7a9686d
Carlos <carlos.appsfactory@gmail.com>
2025-09-13 16:13:51 +0200

    Add TypeORM to places module

--- src/places/places.module.ts
+++ src/places/places.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { PlacesService } from './places.service';
 import { PlacesController } from './places.controller';
+import { Place } from './entities/place.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Trip } from 'src/trips/entities/trip.entity';
 
 @Module({
   controllers: [PlacesController],
   providers: [PlacesService],
+  imports: [
+    TypeOrmModule.forFeature([ Place, Trip ])
+  ]
 })
 export class PlacesModule {}
