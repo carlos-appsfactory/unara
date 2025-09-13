7ae1f3f81e0491593523ffd342a157f8336b9109
Carlos <carlos.appsfactory@gmail.com>
2025-09-13 13:49:46 +0200

    Create trip entity

--- src/trips/entities/trip.entity.ts
+++ src/trips/entities/trip.entity.ts
@@ -1,1 +1,30 @@
-export class Trip {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class Trip {
+    @PrimaryGeneratedColumn('uuid')
+    id: string
+
+    @Column({ type: 'varchar', length: 255 })
+    name: string
+
+    @Column({ type: 'text', nullable: true })
+    description?: string
+
+    @Column({ type: 'text' })
+    destination: string
+
+    @Column({ type: 'timestamptz' })
+    startDate: Date;
+
+    @Column({ type: 'timestamptz' })
+    endDate: Date;
+
+    // TODO: Añadir relación con usuarios
+
+    @CreateDateColumn()
+    createdAt: Date
+
+    @UpdateDateColumn()
+    updatedAt: Date
+}
