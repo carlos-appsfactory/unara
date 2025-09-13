7569c665e45fb1d060903f8b0b06d18525e7f43b
Carlos <carlos.appsfactory@gmail.com>
2025-09-13 16:09:08 +0200

    Create places controller

--- src/places/places.controller.ts
+++ src/places/places.controller.ts
@@ -1,34 +1,51 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, ParseUUIDPipe } from '@nestjs/common';
 import { PlacesService } from './places.service';
 import { CreatePlaceDto } from './dto/create-place.dto';
 import { UpdatePlaceDto } from './dto/update-place.dto';
+import { FilterPlaceDto } from './dto/filter-place.dto';
 
-@Controller('places')
+@Controller('trips/:tripId/places')
 export class PlacesController {
   constructor(private readonly placesService: PlacesService) {}
 
   @Post()
-  create(@Body() createPlaceDto: CreatePlaceDto) {
-    return this.placesService.create(createPlaceDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.placesService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.placesService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePlaceDto: UpdatePlaceDto) {
-    return this.placesService.update(+id, updatePlaceDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.placesService.remove(+id);
-  }
+    create(
+      @Param('tripId', ParseUUIDPipe) tripId: string,
+      @Body() dto: CreatePlaceDto
+    ) {
+      return this.placesService.create(tripId, dto);
+    }
+  
+    @Get()
+    findAll(
+      @Param('tripId', ParseUUIDPipe) tripId: string,
+      @Query() dto: FilterPlaceDto
+    ) {
+      return this.placesService.findAll(tripId, dto);
+    }
+  
+    @Get(':id')
+    findOne(
+      @Param('tripId', ParseUUIDPipe) tripId: string,
+      @Param('id', ParseUUIDPipe) id: string
+    ) {
+      return this.placesService.findOne(tripId, id);
+    }
+    
+    @Patch(':id')
+    update(
+      @Param('tripId', ParseUUIDPipe) tripId: string,
+      @Param('id', ParseUUIDPipe) id: string,
+      @Body() dto: UpdatePlaceDto
+    ) {
+      return this.placesService.update(tripId, id, dto);
+    }
+    
+    @Delete(':id')
+    remove(
+      @Param('tripId', ParseUUIDPipe) tripId: string,
+      @Param('id', ParseUUIDPipe) id: string
+    ) {
+      return this.placesService.remove(tripId, id);
+    }
 }
