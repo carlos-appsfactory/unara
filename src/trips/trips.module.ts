e98abb4cf1fa1269c86306f854d4ae889c70cff1
Carlos <carlos.appsfactory@gmail.com>
2025-09-13 14:24:10 +0200

    Add TypeORM to trips module

--- src/trips/trips.module.ts
+++ src/trips/trips.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TripsService } from './trips.service';
 import { TripsController } from './trips.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Trip } from './entities/trip.entity';
 
 @Module({
   controllers: [TripsController],
   providers: [TripsService],
+  imports: [
+    TypeOrmModule.forFeature([ Trip ])
+  ]
 })
 export class TripsModule {}
