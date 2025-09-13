0bb5fb2e35667ef949f6e66820783c9bedbc1430
Carlos <carlos.appsfactory@gmail.com>
2025-09-13 15:54:59 +0200

    Create create-place DTO

--- src/places/dto/create-place.dto.ts
+++ src/places/dto/create-place.dto.ts
@@ -1,1 +1,23 @@
-export class CreatePlaceDto {}
+import { Type } from "class-transformer";
+import { IsNotEmpty, IsNumber, IsOptional, IsString, IsUUID, MaxLength } from "class-validator";
+
+export class CreatePlaceDto {
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(255)
+    name: string;
+
+    @IsOptional()
+    @IsString()
+    description?: string;
+
+    @IsNotEmpty()
+    @IsNumber()
+    @Type(() => Number)
+    latitude: number;
+
+    @IsNotEmpty()
+    @IsNumber()
+    @Type(() => Number)
+    longitude: number;
+}
