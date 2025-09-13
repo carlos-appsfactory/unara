0e8dec7f7084151b61233af5fabce6ace48a9b24
Carlos <carlos.appsfactory@gmail.com>
2025-09-13 14:22:25 +0200

    Create filter-trip DTO

---
+++ src/trips/dto/filter-trip.dto.ts
@@ -0,0 +1,31 @@
+import { Type } from "class-transformer";
+import { IsDate, IsOptional, IsString, MaxLength } from "class-validator"
+import { PaginationDto } from "src/common/dto/pagination.dto";
+import { IsAfter } from "src/common/validators/is-after.validator";
+
+export class FilterTripDto extends PaginationDto{
+    @IsOptional()
+    @IsString()
+    @MaxLength(255)
+    name?: string
+
+    @IsOptional()
+    @IsString()
+    description?: string;
+
+    @IsOptional()
+    @IsString()
+    @MaxLength(255)
+    destination?: string;
+
+    @IsOptional()
+    @IsDate()
+    @Type(() => Date)
+    startDate?: Date;
+
+    @IsOptional()
+    @IsDate()
+    @Type(() => Date)
+    @IsAfter('startDate')
+    endDate?: Date;
+}
