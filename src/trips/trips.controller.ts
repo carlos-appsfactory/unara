6b631674f83538468cf4d0aea6feeaec10b91d6c
Carlos <carlos.appsfactory@gmail.com>
2025-09-13 14:27:49 +0200

    Create trips controller

--- src/trips/trips.controller.ts
+++ src/trips/trips.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, ParseUUIDPipe } from '@nestjs/common';
 import { TripsService } from './trips.service';
 import { CreateTripDto } from './dto/create-trip.dto';
 import { UpdateTripDto } from './dto/update-trip.dto';
+import { FilterTripDto } from './dto/filter-trip.dto';
 
 @Controller('trips')
 export class TripsController {
@@ -13,22 +14,25 @@ export class TripsController {
   }
 
   @Get()
-  findAll() {
-    return this.tripsService.findAll();
+  findAll(@Query() filterTripDto: FilterTripDto) {
+    return this.tripsService.findAll(filterTripDto);
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.tripsService.findOne(+id);
+    return this.tripsService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTripDto: UpdateTripDto) {
-    return this.tripsService.update(+id, updateTripDto);
+  update(
+    @Param('id', ParseUUIDPipe) id: string, 
+    @Body() updateTripDto: UpdateTripDto
+  ) {
+    return this.tripsService.update(id, updateTripDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.tripsService.remove(+id);
+  remove(@Param('id', ParseUUIDPipe) id: string) {
+    return this.tripsService.remove(id);
   }
 }
