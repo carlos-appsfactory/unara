41faf6f25e6732edcacae03dbecce72b274f4985
Carlos <carlos.appsfactory@gmail.com>
2025-09-13 14:19:26 +0200

    Create create-trip DTO

--- src/trips/dto/create-trip.dto.ts
+++ src/trips/dto/create-trip.dto.ts
@@ -1,1 +1,30 @@
-export class CreateTripDto {}
+import { Type } from "class-transformer";
+import { IsDate, IsNotEmpty, IsOptional, IsString, MaxLength } from "class-validator";
+import { IsAfter } from "src/common/validators/is-after.validator";
+
+export class CreateTripDto {
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(255)
+    name: string;
+
+    @IsOptional()
+    @IsString()
+    description?: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(255)
+    destination: string;
+
+    @IsDate()
+    @Type(() => Date)
+    @IsNotEmpty()
+    startDate: Date;
+
+    @IsDate()
+    @Type(() => Date)
+    @IsNotEmpty()
+    @IsAfter('startDate')
+    endDate: Date;
+}
