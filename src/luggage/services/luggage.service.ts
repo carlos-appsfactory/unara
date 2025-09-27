819ee791bb4657d006babc4690e717a10bd5a8c0
Carlos <carlos.appsfactory@gmail.com>
2025-09-27 17:56:45 +0200

    Remove CategoryId from luggage service

--- src/luggage/services/luggage.service.ts
+++ src/luggage/services/luggage.service.ts
@@ -1,7 +1,6 @@
 import { Injectable, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { LuggageCategory } from 'src/luggage-categories/entities/luggage-category.entity';
 import { Luggage } from '../entities/luggage.entity';
 import { CreateLuggageDto } from '../dto/create-luggage.dto';
 import { FilterLuggageDto } from '../dto/filter-luggage.dto';
@@ -15,59 +14,18 @@ export class LuggageService {
     @InjectRepository(Luggage)
     private readonly luggageRepository: Repository<Luggage>,
 
-    @InjectRepository(LuggageCategory)
-    private readonly luggageCategoryRepository: Repository<LuggageCategory>,
-
     @InjectRepository(Trip)
     private readonly tripRepository: Repository<Trip>,
   ){}
 
-<<<<<<< HEAD
-  async create(createLuggageDto: CreateLuggageDto) {
-<<<<<<< HEAD
-    try {
-      const { categoryId, tripId, ...luggageData } = createLuggageDto
-=======
-    const { categoryId, ...luggageData } = createLuggageDto
->>>>>>> 18a7002 (Remove db filter handler from luggage service)
-=======
+
   async create(dto: CreateLuggageDto) {
-    const { categoryId, ...luggageData } = dto
->>>>>>> c827588 (Change DTO complex names to DTO on luggage service)
-
-    const category = await this.luggageCategoryRepository.findOneBy({ id: categoryId })
-
-    if (!category) throw new NotFoundException(`Category with id ${categoryId} not found`)
-
-<<<<<<< HEAD
-      let trip: Trip | null = null;
-      if (tripId) {
-        trip = await this.tripRepository.findOneBy({ id: tripId })
-
-        if (!trip) throw new NotFoundException(`Trip with id ${tripId} not found`)
-      }
-
-      const luggage = this.luggageRepository.create({
-        ...luggageData,
-        category,
-        ...(trip ? { trip } : {}),
-      })
-      
-      await this.luggageRepository.save(luggage)
-      return luggage
-
-    } catch (error) {
-      this.handleExceptions(error)
-    }
-=======
     const luggage = this.luggageRepository.create({
-      ...luggageData,
-      category
+      ...dto
     })
     
     await this.luggageRepository.save(luggage)
     return luggage
->>>>>>> 18a7002 (Remove db filter handler from luggage service)
   }
 
   async findAll(dto: FilterLuggageDto) {
@@ -75,22 +33,13 @@ export class LuggageService {
       limit = 10, 
       offset = 0,
       name,
-      categoryId,
-<<<<<<< HEAD
       tripId,
-    } = filterLuggageDto
-=======
     } = dto
->>>>>>> c827588 (Change DTO complex names to DTO on luggage service)
 
     const query = this.luggageRepository
                     .createQueryBuilder('luggage')
-                    .leftJoinAndSelect('luggage.category', 'category')
-                    .leftJoinAndSelect('luggage.trip', 'trip')
 
     if (name) query.andWhere('luggage.name ILIKE :name', { name: `%${name}%`})
-    
-    if (categoryId) query.andWhere('category.id = :categoryId', { categoryId})
 
     if (tripId) query.andWhere('trip.id = :tripId', { tripId })
 
@@ -101,8 +50,7 @@ export class LuggageService {
 
   async findOne(id: string) {
     const luggage = await this.luggageRepository.findOne({
-      where: { id },
-      relations: {  category: true }
+      where: { id }
     })
 
     if (!luggage){
@@ -112,20 +60,8 @@ export class LuggageService {
     return luggage
   }
 
-<<<<<<< HEAD
   async update(id: string, updateLuggageDto: UpdateLuggageDto) {
-    const { categoryId, tripId, ...luggageData } = updateLuggageDto;
-=======
-  async update(id: string, dto: UpdateLuggageDto) {
-    const { categoryId, ...luggageData } = dto;
->>>>>>> c827588 (Change DTO complex names to DTO on luggage service)
-
-    let category: LuggageCategory | null = null;
-    if (categoryId) {
-      category = await this.luggageCategoryRepository.findOneBy({ id: categoryId })
-
-      if (!category) throw new NotFoundException(`Category with id ${categoryId} not found`)
-    }
+    const { tripId, ...luggageData } = updateLuggageDto;
 
     let trip: Trip | null = null;
     if (tripId) {
@@ -137,7 +73,6 @@ export class LuggageService {
     const luggage = await this.luggageRepository.preload({
       id,
       ...luggageData,
-      ...(category ? { category } : {}),
       ...(trip ? { trip } : {}),
     })
 
