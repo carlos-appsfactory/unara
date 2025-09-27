7b0c7c28e012a95458c0d746cb356b93faf0278d
Carlos <carlos.appsfactory@gmail.com>
2025-09-27 21:04:04 +0200

    Create controller for activity

--- src/activities/activities.controller.ts
+++ src/activities/activities.controller.ts
@@ -1,34 +1,40 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseFilters, Query, ParseUUIDPipe } from '@nestjs/common';
 import { ActivitiesService } from './activities.service';
 import { CreateActivityDto } from './dto/create-activity.dto';
 import { UpdateActivityDto } from './dto/update-activity.dto';
+import { DatabaseExceptionFilter } from 'src/common/filters/db-exception.filter';
+import { FilterActivityDto } from './dto/filter-activity.dto';
 
+@UseFilters(new DatabaseExceptionFilter('Activities'))
 @Controller('activities')
 export class ActivitiesController {
   constructor(private readonly activitiesService: ActivitiesService) {}
 
   @Post()
-  create(@Body() createActivityDto: CreateActivityDto) {
-    return this.activitiesService.create(createActivityDto);
+  create(@Body() dto: CreateActivityDto) {
+    return this.activitiesService.create(dto);
   }
 
   @Get()
-  findAll() {
-    return this.activitiesService.findAll();
+  findAll(@Query() dto: FilterActivityDto) {
+    return this.activitiesService.findAll(dto);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.activitiesService.findOne(+id);
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.activitiesService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateActivityDto: UpdateActivityDto) {
-    return this.activitiesService.update(+id, updateActivityDto);
+  update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() dto: UpdateActivityDto
+  ) {
+    return this.activitiesService.update(id, dto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.activitiesService.remove(+id);
+  remove(@Param('id', ParseUUIDPipe) id: string) {
+    return this.activitiesService.remove(id);
   }
 }
