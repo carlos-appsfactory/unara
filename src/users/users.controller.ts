eb9ff94aff91952ebfafb2a9a43b130c60f2d511
Carlos <carlos.appsfactory@gmail.com>
2025-09-27 18:51:22 +0200

    Add db filter handler to users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe, Query, UseFilters } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { FilterUserDto } from './dto/filter-user.dto';
+import { DatabaseExceptionFilter } from 'src/common/filters/db-exception.filter';
 
+@UseFilters(new DatabaseExceptionFilter('Users'))
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
