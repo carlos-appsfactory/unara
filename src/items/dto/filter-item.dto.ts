4c9b981d3141091ae017e8cfc7ffe15c70b9b34a
Carlos <carlos.appsfactory@gmail.com>
2025-09-27 17:31:08 +0200

    Add description to item filter

--- src/items/dto/filter-item.dto.ts
+++ src/items/dto/filter-item.dto.ts
@@ -1,11 +1,15 @@
-import { IsOptional, IsString, IsUUID, MaxLength } from "class-validator";
-import { PaginationDto } from "src/common/dto/pagination.dto";
+import { IsOptional, IsString, IsUUID, MaxLength } from "class-validator"
+import { PaginationDto } from "src/common/dto/pagination.dto"
 
 export class FilterItemDto extends PaginationDto{
     @IsOptional()
     @IsString()
     @MaxLength(255)
     name?: string
+
+    @IsOptional()
+    @IsString()
+    description?: string
 
     @IsOptional()
     @IsUUID()
