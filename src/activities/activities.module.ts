065cd6c324eb06db8bca3f6bd1bbdf5772e2528e
Carlos <carlos.appsfactory@gmail.com>
2025-09-27 19:46:45 +0200

    Add TypeORM to activities module

--- src/activities/activities.module.ts
+++ src/activities/activities.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ActivitiesService } from './activities.service';
 import { ActivitiesController } from './activities.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Activity } from './entities/activity.entity';
+import { Trip } from 'src/trips/entities/trip.entity';
+import { Place } from 'src/places/entities/place.entity';
 
 @Module({
   controllers: [ActivitiesController],
   providers: [ActivitiesService],
+  imports: [
+    TypeOrmModule.forFeature([ Activity, Trip, Place ])
+  ]
 })
 export class ActivitiesModule {}
