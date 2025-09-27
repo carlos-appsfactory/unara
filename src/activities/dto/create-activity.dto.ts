8f71ca48e6db0b09d1d2fe0644d9827e5e0e1cfe
Carlos <carlos.appsfactory@gmail.com>
2025-09-27 20:34:30 +0200

    Create DTOs for activity

--- src/activities/dto/create-activity.dto.ts
+++ src/activities/dto/create-activity.dto.ts
@@ -1,1 +1,26 @@
-export class CreateActivityDto {}
+import { Type } from "class-transformer";
+import { IsDate, IsNotEmpty, IsOptional, IsString, IsUUID, MaxLength } from "class-validator";
+
+export class CreateActivityDto {
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(255)
+    name: string
+
+    @IsOptional()
+    @IsString()
+    description?: string
+
+    @IsDate()
+    @Type(() => Date)
+    date: string
+
+    @IsUUID()
+    @IsNotEmpty()
+    tripId: string
+
+    @IsOptional()
+    @IsUUID()
+    @IsNotEmpty()
+    placeId?: string
+}
---
+++ src/activities/dto/filter-activity.dto.ts
@@ -0,0 +1,32 @@
+import { IsDate, IsOptional, IsString, IsUUID, MaxLength } from "class-validator"
+import { Type } from "class-transformer"
+import { PaginationDto } from "src/common/dto/pagination.dto"
+
+export class FilterActivityDto extends PaginationDto {
+  @IsOptional()
+  @IsString()
+  @MaxLength(255)
+  name?: string
+
+  @IsOptional()
+  @IsString()
+  description?: string
+
+  @IsOptional()
+  @IsDate()
+  @Type(() => Date)
+  dateFrom?: Date
+
+  @IsOptional()
+  @IsDate()
+  @Type(() => Date)
+  dateTo?: Date
+
+  @IsOptional()
+  @IsUUID()
+  tripId?: string
+
+  @IsOptional()
+  @IsUUID()
+  placeId?: string
+}
