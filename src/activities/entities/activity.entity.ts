5f6b0c8ae15cb0b434d369eff0374dea407930d7
Carlos <carlos.appsfactory@gmail.com>
2025-09-27 20:11:41 +0200

    Create activity entity

--- src/activities/entities/activity.entity.ts
+++ src/activities/entities/activity.entity.ts
@@ -1,1 +1,33 @@
-export class Activity {}
+import { Place } from "src/places/entities/place.entity";
+import { Trip } from "src/trips/entities/trip.entity";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Activity {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string
+
+    @Column({ type: 'varchar', length: 255 })
+    name: string
+    
+    @Column({ type: 'text', nullable: true })
+    description?: string
+
+    @Column({ type: 'timestamp' })
+    date: Date
+
+    @ManyToOne(
+        () => Trip, 
+        trip => trip.activities,
+        { onDelete: 'CASCADE' }
+    )
+    trip: Trip
+
+    @ManyToOne(
+        () => Place, 
+        place => place.activities,
+        { onDelete: 'CASCADE' }
+    )
+    place?: Place
+}
