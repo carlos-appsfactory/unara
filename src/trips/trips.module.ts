409ed0365ab3846b497552b9488486d305a0a0e9
Carlos <carlos.appsfactory@gmail.com>
2025-09-28 19:03:45 +0200

    Modify trips service to add users

--- src/trips/trips.module.ts
+++ src/trips/trips.module.ts
@@ -3,12 +3,13 @@ import { TripsService } from './trips.service';
 import { TripsController } from './trips.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Trip } from './entities/trip.entity';
+import { User } from 'src/users/entities/user.entity';
 
 @Module({
   controllers: [TripsController],
   providers: [TripsService],
   imports: [
-    TypeOrmModule.forFeature([ Trip ])
+    TypeOrmModule.forFeature([ Trip, User ])
   ]
 })
 export class TripsModule {}
--- src/trips/trips.service.ts
+++ src/trips/trips.service.ts
@@ -3,73 +3,112 @@ import { CreateTripDto } from './dto/create-trip.dto';
 import { UpdateTripDto } from './dto/update-trip.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Trip } from './entities/trip.entity';
-import { Repository } from 'typeorm';
+import { In, Repository } from 'typeorm';
 import { FilterTripDto } from './dto/filter-trip.dto';
+import { User } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class TripsService {
 
   constructor(
+    
     @InjectRepository(Trip)
-    private readonly tripRepository: Repository<Trip>
+    private readonly tripRepository: Repository<Trip>,
+
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>
   ){}
 
   async create(dto: CreateTripDto) {
-    const trip = this.tripRepository.create(dto)
-    await this.tripRepository.save(trip)
-    return trip
+      const { userIds, ...tripData } = dto;
+
+      const users = await this.userRepository.find({
+          where: { id: In(userIds) }
+      });
+
+      if (users.length !== userIds.length) {
+          throw new NotFoundException("Some users not found");
+      }
+
+      const trip = this.tripRepository.create({
+          ...tripData,
+          users: users
+      });
+
+      await this.tripRepository.save(trip);
+      return trip;
   }
 
   async findAll(dto: FilterTripDto) {
-    const { 
-      limit = 10, 
-      offset = 0,
-      name,
-      description, 
-      destination,
-      startDate,
-      endDate
-    } = dto
+      const { 
+        limit = 10, 
+        offset = 0,
+        name,
+        description, 
+        destination,
+        startDate,
+        endDate
+      } = dto;
 
-    const query = this.tripRepository.createQueryBuilder('trip')
+      const query = this.tripRepository
+                        .createQueryBuilder('trip')
+                        .leftJoinAndSelect('trip.users', 'user');
 
-    if (name) query.andWhere('trip.name ILIKE :name', { name: `%${name}%`})
+      if (name) query.andWhere('trip.name ILIKE :name', { name: `%${name}%` });
+      if (description) query.andWhere('trip.description ILIKE :description', { description: `%${description}%` });
+      if (destination) query.andWhere('trip.destination ILIKE :destination', { destination: `%${destination}%` });
+      if (startDate) query.andWhere('trip.startDate >= :startDate', { startDate });
+      if (endDate) query.andWhere('trip.endDate <= :endDate', { endDate });
 
-    if (description) query.andWhere('trip.description ILIKE :description', { description: `%${description}%`})
+      query.skip(offset).take(limit);
 
-    if (destination) query.andWhere('trip.destination ILIKE :destination', { destination: `%${destination}%`})
-    
-    if (startDate) query.andWhere('trip.startDate >= :startDate', { startDate });
-
-    if (endDate) query.andWhere('trip.endDate <= :endDate', { endDate });
-
-    query.skip(offset).take(limit)
-
-    return query.getMany()
+      return query.getMany();
   }
 
   async findOne(id: string) {
-    const trip = await this.tripRepository.findOneBy({ id })
+      const trip = await this.tripRepository.findOne({
+          where: { id },
+          relations: {
+              users: true,
+              places: true,
+              activities: true,
+              luggage: true
+          }
+      });
 
-    if (!trip){
-      throw new NotFoundException(`Trip with id ${id} not found`)
-    }
+      if (!trip) {
+          throw new NotFoundException(`Trip with id ${id} not found`);
+      }
 
-    return trip
+      return trip;
   }
 
   async update(id: string, dto: UpdateTripDto) {
-    const trip = await this.tripRepository.preload({
-      id,
-      ...dto
-    })
+      const { userIds, ...tripData } = dto;
 
-    if (!trip){
-      throw new NotFoundException(`Trip with id ${id} not found`)
-    }
+      let users: User[] | null = null;
+      if (userIds) {
+          users = await this.userRepository.find({
+              where: { id: In(userIds) }
+          });
 
-    await this.tripRepository.save(trip)
-    return trip
+          if (users.length !== userIds.length) {
+              throw new NotFoundException("Some users not found");
+          }
+      }
+
+      const trip = await this.tripRepository.preload({
+          id,
+          ...tripData,
+          ...(users ? { users } : {})
+      });
+
+      if (!trip) {
+          throw new NotFoundException(`Trip with id ${id} not found`);
+      }
+
+      await this.tripRepository.save(trip);
+      return trip;
   }
 
   async remove(id: string) {
